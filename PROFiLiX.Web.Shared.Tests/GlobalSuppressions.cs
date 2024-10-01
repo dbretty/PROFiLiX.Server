362956634eb8554ca7b6f40ead20116ebd8362f8
Dave Brett <dave@bretty.me.uk>
2024-10-01 11:35:22 +0100

    NUnit Update

---
+++ PROFiLiX.Web.Shared.Tests/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// <copyright file="GlobalSuppressions.cs" company="bretty.me.uk">
+// Copyright (c) bretty.me.uk. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Not relevant in this solution")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1309:Field names should not begin with underscore", Justification = "Required in this solution")]
